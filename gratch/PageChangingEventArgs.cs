bbbd661d8e64fd1ba85300649ce50e69d06b5d69
Alex <fox228.af@gmail.com>
2021-05-05 23:20:21 +0300

    PageChangingEventArgs added

---
+++ gratch/PageChangingEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gratch
+{
+    public class PageChangingEventArgs : EventArgs
+    {
+        //<summary>
+        //Hello World   
+        //</summary>
+        public PageChangingEventArgs(Xamarin.Forms.Page targetpage)
+        {
+            TargetPage = targetpage;
+        }
+
+        public Xamarin.Forms.Page TargetPage { get; set; }
+    }
+}
